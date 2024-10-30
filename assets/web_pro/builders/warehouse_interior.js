61ad71cd8e5366f7f4bc60bef4f7012b06baad4f
madhankumar-ch <chiluverimadhankumarnetha@gmail.com>
2024-10-30 17:55:25 +0530

    changed areas logic

--- assets/web_pro/builders/warehouse_interior.js
+++ assets/web_pro/builders/warehouse_interior.js
@@ -2,17 +2,24 @@ import * as THREE from 'three';
 import { getPlaneGeometry } from "plane";
 
 export function buildAreas(json, scene, warehouseCenter, width, depth){
-    let previousAreaPosition = new THREE.Vector3(warehouseCenter.x-width/2+json['areas'][0]['width']/2, warehouseCenter.y+0.05, warehouseCenter.z-depth/2+json['areas'][0]['depth']/2);
+    let nextAreaPosition = new THREE.Vector3(warehouseCenter.x-width/2+json['areas'][0]['width']/2, warehouseCenter.y+0.05, warehouseCenter.z-depth/2+json['areas'][0]['depth']/2);
     let coveredWidth = json['areas'][0]['width'];
+    let index = 
 
-    json['areas'].forEach(item => {
+    json['areas'].forEach((item, index) => {
         const area = getPlaneGeometry(item['width'], item['depth']);
         console.log('coveredWidth '+coveredWidth+' width '+width);
-        // if(coveredWidth < width){
-            area.position.set(previousAreaPosition.x, previousAreaPosition.y, previousAreaPosition.z);
+        if(coveredWidth <= width){
+            area.position.set(nextAreaPosition.x, nextAreaPosition.y, nextAreaPosition.z);
             scene.add(area);
-            previousAreaPosition.x = previousAreaPosition.x + coveredWidth/2 + item['width']/2;
-            coveredWidth = coveredWidth + item['width'];
-        // }
+            nextAreaPosition.x = nextAreaPosition.x + item['width']/2+json['areas'][index+1]['width']/2;
+            console.log('index '+ index+ 'length ' +json['areas'].length);
+            if(index < json['areas'].length){
+                coveredWidth = coveredWidth + json['areas'][index+1]['width'];
+            }
+        }else{
+            console.log(warehouseCenter.x + " " + warehouseCenter.y + " " + warehouseCenter.z);
+            console.log(nextAreaPosition.x + " " + nextAreaPosition.y + " " + nextAreaPosition.z);
+        }
     });
 }
